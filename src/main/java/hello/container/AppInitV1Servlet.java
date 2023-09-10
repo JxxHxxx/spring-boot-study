b69156f8555c3966b041e4e82b0614d29df8e7e6
JxxHxxx <leesin5498@naver.com>
2023-09-10 17:13:24 +0900

    외장 톰캣 마무리

--- src/main/java/hello/container/AppInitV1Servlet.java
+++ src/main/java/hello/container/AppInitV1Servlet.java
@@ -6,9 +6,9 @@ import jakarta.servlet.ServletContext;
 import jakarta.servlet.ServletRegistration;
 
 // 애플리케이션 초기화
-public class AppInitV1Servlet implements AppInit{
-
-    @Override
+public class AppInitV1Servlet // implements AppInit {
+{
+//    @Override
     public void onStartup(ServletContext servletContext) {
         System.out.println("AppInitV1Servlet.onStartup");
 
